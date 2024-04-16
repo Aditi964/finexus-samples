fe0a36fdfe7dc981e3ba79759868477125532b23
Aditi964 <104507814+Aditi964@users.noreply.github.com>
2024-04-16 15:39:38 +0530

    Add files via upload

---
+++ asset-transfer-scm/chaincode-go/assetTransfer.go
@@ -0,0 +1,25 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/hyperledger/fabric-samples/asset-transfer-scm/chaincode-go/chaincode"
+)
+
+// Define main function for testing
+func main() {
+	chaincode, err := contractapi.NewChaincode(&SmartContract{})
+	if err != nil {
+		fmt.Printf("Error creating asset-transfer-scm chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting asset-transfer-scm chaincode: %s", err.Error())
+	}
+}
